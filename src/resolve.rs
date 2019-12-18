62b3b1453562890ae12fc362f214f0034caf5726
Sam Collard <swift@swifteagle.co.uk>
2019-12-18 11:28:53 +0000

    Resolution test

--- src/resolve.rs
+++ src/resolve.rs
@@ -3,3 +3,21 @@ use crate::types::{Outcome, Ship};
 pub fn resolve(a: &Ship, b: &Ship) -> Outcome {
   a.strongs.cmp(&b.strongs)
 }
+
+#[cfg(test)]
+mod tests {
+  use super::{resolve, Outcome, Ship};
+
+  #[test]
+  fn stronger_ship_wins() {
+    let muscle_and_gainz = Ship {
+      name: "Muscle and Gainz".to_string(),
+      strongs: 284,
+    };
+    let tiny_baby = Ship {
+      name: "Tiny Baby".to_string(),
+      strongs: 2,
+    };
+    assert_eq!(resolve(&muscle_and_gainz, &tiny_baby), Outcome::Greater);
+  }
+}
